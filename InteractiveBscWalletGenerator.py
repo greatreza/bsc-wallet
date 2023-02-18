e98827e682ef6522b7e373a955f2d6e1f4b3fe99
reza.zeinali <reza.zeinali21@gmail.com>
2023-02-18 14:41:18 +0330

    init commit

---
+++ InteractiveBscWalletGenerator.py
@@ -0,0 +1,25 @@
+from bip_utils import Bip39MnemonicGenerator, Bip39SeedGenerator, Bip44, Bip44Coins, Bip44Changes
+
+a = Bip39MnemonicGenerator()
+
+mnemonic = a.FromWordsNumber(words_num=int(input("please enter number of mnemonic words you want 12 or 24?")))
+print(f"Mnemonic string:\n{mnemonic}")
+
+seed_bytes = Bip39SeedGenerator(mnemonic).Generate()
+
+# Generate BIP44 master keys
+bip_obj_mst = Bip44.FromSeed(seed_bytes, Bip44Coins.BINANCE_SMART_CHAIN)
+
+# Generate BIP44 account keys
+bip_obj_acc = bip_obj_mst.Purpose().Coin().Account(0)
+
+# Generate BIP44 chain keys
+bip_obj_chain = bip_obj_acc.Change(Bip44Changes.CHAIN_EXT)
+
+# Generate the address pool (first 1 address)
+for i in range(int(input("please enter number of wallets that you need:"))):
+    bip_obj_addr = bip_obj_chain.AddressIndex(i)
+    print(f"{i+1}. Address private key:\n{bip_obj_addr.PrivateKey().Raw()}")
+    print(f"{i+1}. Address:\n{bip_obj_addr.PublicKey().ToAddress()}")
+    print("Now you can import this PRIVATE KEY or MNEMONIC to your metamask or trust wallet and deposit your BSC "
+          "tokens like CAKE to your generated address ")
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # bsc-wallet
+In this repo i made an interactive script that makes any number of bsc wallets that you need.
+
+```shell
+python -m venv venv
+```
+```shell
+pip install -r requirements.txt
+```
+
+there are three versions in this repo:
+* ipynb file for editing and using each part separately
+* BscWalletGenerator file for generating 1 bsc wallet with 12 words mnemonic
+* InteractiveBscWalletGenerator file for generating multiple wallets with 12 or 24 words mnemonic
+
+run each version that you need with:
+```shell
+python file.py
+```
+for ipynb open it in jupyter or jupyter-lab
